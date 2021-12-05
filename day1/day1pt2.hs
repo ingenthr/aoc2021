1ad41533ae0243c1b9bfc381d34585684564db61
Matt Ingenthron <ingenthr@cep.net>
2021-12-05 00:11:12 -0800

    d1p2 wip

--- day1/day1pt2.hs
+++ day1/day1pt2.hs
@@ -1,9 +1,15 @@
 import System.IO
 
-compareMine:: [Int] -> [Int] -> Int
-compareMine a b =
-  sum listRes where
-    listRes = zipWith shouldAdd a b
+compareMine:: [Int] -> [Int] -> Int -> Int
+compareMine a b total =
+  compareMine (tail a) (tail b) windowRes where
+    windowRes = compareWindows a (tail b)
+
+compareWindows:: [Int] -> [Int] -> Int
+compareWindows first second =
+  shouldAdd sumFirst sumSecond where
+    sumFirst = sum (take 3 first)
+    sumSecond = sum (take 3 second)
 
 shouldAdd:: Int -> Int -> Int
 shouldAdd a b =
@@ -15,7 +21,7 @@ shouldAdd a b =
 
 solve:: [Int] -> Int
 solve list =
-  compareMine list (tail list)
+  compareMine list (tail list) 0
 
 
 getInput :: IO [Int]
