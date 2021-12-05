ba9d8b39f0f37aed6c41e95f5bea7faee9a19780
Matt Ingenthron <ingenthr@cep.net>
2021-12-04 23:00:32 -0800

    Start on d1p2.

---
+++ day1/day1pt2.hs
@@ -0,0 +1,26 @@
+import System.IO
+
+compareMine:: [Int] -> [Int] -> Int
+compareMine a b =
+  sum listRes where
+    listRes = zipWith shouldAdd a b
+
+shouldAdd:: Int -> Int -> Int
+shouldAdd a b =
+  if a < b
+    then
+     1
+    else
+     0
+
+solve:: [Int] -> Int
+solve list =
+  compareMine list (tail list)
+
+
+getInput :: IO [Int]
+getInput = fmap read.lines <$> readFile "input.txt"
+
+
+main =
+  getInput >>= \input -> print (solve input)
